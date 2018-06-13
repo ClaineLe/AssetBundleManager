13209c0f21b50c674fa072ff13076b447a36a79d
ClaineLe <7941324@qq.com>
2018-06-13 14:14:34 +0800

    1.升级Unity至2017.4.3f1版本 2.修复升级后带来的代码异常 3.增加忽略文件目录

--- Assets/AssetBundleManager/Editor/BuildScript.cs
+++ Assets/AssetBundleManager/Editor/BuildScript.cs
@@ -173,10 +173,8 @@ namespace AssetBundles
                     return "/test.exe";
                 case BuildTarget.StandaloneOSXIntel:
                 case BuildTarget.StandaloneOSXIntel64:
-                case BuildTarget.StandaloneOSXUniversal:
+                case BuildTarget.StandaloneOSX:
                     return "/test.app";
-                case BuildTarget.WebPlayer:
-                case BuildTarget.WebPlayerStreamed:
                 case BuildTarget.WebGL:
                 case BuildTarget.iOS:
                     return "";
